9d3346e42a90db1e8b9426e62f3819123951c7df
Dev123dahiya <161199960+Dev123dahiya@users.noreply.github.com>
2026-05-26 15:09:48 +0530

    Delete golden_response/config.py

--- golden_response/config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = "benchmark-dev-secret-change-in-production"
-TOKEN_TTL_SECONDS = 7 * 24 * 60 * 60
-DATE_FORMAT = "%Y-%m-%d"
-
-LEAVE_TYPES = {"annual", "sick", "casual"}
-ROLES = {"employee", "manager", "admin"}
